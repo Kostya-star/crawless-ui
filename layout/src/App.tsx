5efafafba1d10130b16028a8c5b8e2acd19088e2
Kostya-star <konstantindanilov19@hotmail.com>
2023-08-09 21:11:46 +0300

    fix: fix index tsx

--- layout/src/App.tsx
+++ layout/src/App.tsx
@@ -1,12 +1,22 @@
 import { Hero } from 'components/Hero/Hero';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation, useNavigate } from 'react-router-dom';
 import './scss/main.scss';
 import { TasksPage } from 'pages/TasksPage';
 import { DocumentationPage } from 'pages/DocumentationPage';
 import { SettingsPage } from 'pages/SettingsPage';
 import { IssuesPage } from 'pages/IssuesPage';
+import { useEffect } from 'react';
 
 export const App = () => {
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (pathname === '/') {
+      navigate('/tasks/documentation');
+    }
+  }, [pathname, navigate]);
+  
   return (
     <Routes>
       <Route path="/" element={<Hero />}>
